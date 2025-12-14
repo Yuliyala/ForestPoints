7517508280007fb635b6052e4ff3f5c4f132536e
Yuliyala <yuliyalapenak@gmail.com>
2025-12-14 09:44:11 +0300

    Initial commit

---
+++ ForestPoints/ForestPointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestPointsApp.swift
+//  ForestPoints
+//
+//  Created by Yuliya Lapenak on 12/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ForestPointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
